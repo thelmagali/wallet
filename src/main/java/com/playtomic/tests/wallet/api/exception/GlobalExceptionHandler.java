9a1cd1957dc5eee67f914db30461a9533a4bf33f
Thelma Balbuena <thelmabalbuena@gmail.com>
2024-03-21 12:20:00 +0100

    Better logging

--- src/main/java/com/playtomic/tests/wallet/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/playtomic/tests/wallet/api/exception/GlobalExceptionHandler.java
@@ -6,12 +6,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAllExceptions(final Exception ex) {
+        logger.error("ERROR: ", ex);
         ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 ex.getMessage());
@@ -20,6 +24,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(WalletNotFoundException.class)
     public ResponseEntity<Object> handleWalletNotFoundException(final WalletNotFoundException ex) {
+        logger.error("Wallet not found: ", ex);
         ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
                 ex.getMessage());
